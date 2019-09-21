c6e6cf0269361f261cc0f3a4aa8cf5450fc14177
Peter allain <pballain910@gmail.com>
2019-09-21 17:49:35 -0400

    committing progress

--- pricing.js
+++ pricing.js
@@ -1,7 +1,18 @@
+function employerContribution(dollarsOff, product, price) {
+  if (product.employerContribution.mode === 'dollar') {
+    price = price - product.employerContribution.contribution
+  } else {
+    dollarsOff = price * (product.employerContribution.contribution / 100)
+    price = price - dollarsOff
+  }
+  return price
+}
+function formatPrice(price) {
+  return parseInt(price * 100) / 100
+}
 module.exports.calculateProductPrice = function (product, employee, coverageLevels) {
   var price = 0
   var dollarsOff = 0
-
   switch (product.type) {
     case 'volLife':
       for (var i = 0; i < coverageLevels.length; i++) {
@@ -10,27 +21,17 @@ module.exports.calculateProductPrice = function (product, employee, coverageLeve
         price += (coverageAmount / product.cost.costDivisor) * product.cost.price
       }
 
-      if (product.employerContribution.mode === 'dollar') {
-        price = price - product.employerContribution.contribution
-      } else {
-        dollarsOff = price * (product.employerContribution.contribution / 100)
-        price = price - dollarsOff
-      }
+      var newPrice = employerContribution(dollarsOff, product, price)
 
-      return parseInt(price * 100) / 100
+      return formatPrice(newPrice)
     case 'ltd':
       var salaryPercentage = product.coveragePercentage / 100
 
       price += ((employee.salary * salaryPercentage) / product.cost.costDivisor) * product.cost.price
 
-      if (product.employerContribution.mode === 'dollar') {
-        price = price - product.employerContribution.contribution
-      } else {
-        dollarsOff = price * product.employerContribution.contribution
-        price = price - dollarsOff
-      }
+      var discountPrice = employerContribution(dollarsOff, product, price)
 
-      return parseInt(price * 100) / 100
+      return formatPrice(discountPrice)
     default:
       return 0
   }
